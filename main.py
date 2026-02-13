16218c5c9a0dcb7f788bf19b713a1a1e8696b833
Utkarsh736 <utkarshtomar736@gmail.com>
2026-02-13 13:18:39 +0530

    [Add] Point and Line classes with drawing capability

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+    
+    # Draw an X across the window
+    line1 = Line(Point(0, 0), Point(800, 600))
+    win.draw_line(line1, "black")
+    
+    line2 = Line(Point(800, 0), Point(0, 600))
+    win.draw_line(line2, "red")
+    
+    # Draw a triangle
+    line3 = Line(Point(400, 100), Point(200, 400))
+    win.draw_line(line3, "blue")
+    
+    line4 = Line(Point(200, 400), Point(600, 400))
+    win.draw_line(line4, "green")
+    
+    line5 = Line(Point(600, 400), Point(400, 100))
+    win.draw_line(line5, "purple")
+    
     win.wait_for_close()
 
 
