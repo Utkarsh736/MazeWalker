4a5984f7080b4e8062e39e2e36948c2091daa464
Utkarsh736 <utkarshtomar736@gmail.com>
2026-02-13 13:57:42 +0530

    Implement DFS maze solver with backtracking visualization

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from maze import Maze
 def main():
     win = Window(800, 600)
     
-    # No seed = different maze every time!
-    maze = Maze(100, 60, 12, 15, 40, 40, win)
+    # Create maze with seed for reproducible testing
+    maze = Maze(100, 60, 12, 15, 40, 40, win, seed=0)
+    
+    # Solve the maze!
+    is_solveable = maze.solve()
+    
+    if is_solveable:
+        print("Maze solved! 🎉")
+    else:
+        print("Maze cannot be solved 😢")
     
     win.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -32,6 +32,7 @@ class Maze:
         self.__create_cells()
         self.__break_entrance_and_exit()
         self.__break_walls_r(0, 0)  # Start from top-left
+        self.__reset_cells_visited()
 
 
     
@@ -132,4 +133,78 @@ class Maze:
             
             # Recursively visit the chosen cell
             self.__break_walls_r(next_i, next_j)
+
+    def __reset_cells_visited(self):
+        for col in self.__cells:
+            for cell in col:
+                cell.visited = False
+
+    def solve(self):
+        return self._solve_r(0, 0)
+    
+    def _solve_r(self, i, j):
+        # Animate the current step
+        self.__animate()
+        
+        # Mark current cell as visited
+        self.__cells[i][j].visited = True
+        
+        # Check if we reached the end (bottom-right cell)
+        if i == self.__num_cols - 1 and j == self.__num_rows - 1:
+            return True
+        
+        # Try all four directions: left, right, up, down
+        
+        # Move left
+        if (
+            i > 0 
+            and not self.__cells[i][j].has_left_wall 
+            and not self.__cells[i - 1][j].visited
+        ):
+            self.__cells[i][j].draw_move(self.__cells[i - 1][j])
+            if self._solve_r(i - 1, j):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i - 1][j], undo=True)
+        
+        # Move right
+        if (
+            i < self.__num_cols - 1 
+            and not self.__cells[i][j].has_right_wall 
+            and not self.__cells[i + 1][j].visited
+        ):
+            self.__cells[i][j].draw_move(self.__cells[i + 1][j])
+            if self._solve_r(i + 1, j):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i + 1][j], undo=True)
+        
+        # Move up
+        if (
+            j > 0 
+            and not self.__cells[i][j].has_top_wall 
+            and not self.__cells[i][j - 1].visited
+        ):
+            self.__cells[i][j].draw_move(self.__cells[i][j - 1])
+            if self._solve_r(i, j - 1):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i][j - 1], undo=True)
+        
+        # Move down
+        if (
+            j < self.__num_rows - 1 
+            and not self.__cells[i][j].has_bottom_wall 
+            and not self.__cells[i][j + 1].visited
+        ):
+            self.__cells[i][j].draw_move(self.__cells[i][j + 1])
+            if self._solve_r(i, j + 1):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i][j + 1], undo=True)
+        
+        # No valid path found from this cell
+        return False
+
+
 
